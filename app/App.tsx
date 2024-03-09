44238bd10c82cbd243b02027c9c7f4e32763a119
Lucas Lois <lucasloisp@gmail.com>
2024-03-09 13:53:47 -0300

    set up theme

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { ThemeProvider } from "@shopify/restyle";
 
 import { Router } from "./src/app/navigation/Router";
+import theme from "./src/ui/theme";
 
 export default function App() {
 	return (
-		<NavigationContainer>
-			<Router />
-		</NavigationContainer>
+		<ThemeProvider theme={theme}>
+			<NavigationContainer>
+				<Router />
+			</NavigationContainer>
+		</ThemeProvider>
 	);
 }
