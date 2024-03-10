ddacc954d36e9ce8f1cfca366ae67f4775c69311
Lucas Lois <lucasloisp@gmail.com>
2024-03-10 20:16:21 -0300

    shows recipe details on method and coffee size & amount

--- app/src/app/navigation/screens/BrewScreen.tsx
+++ app/src/app/navigation/screens/BrewScreen.tsx
@@ -1,6 +1,8 @@
 import { Linking, Pressable } from "react-native";
 
 import AeroPressIcon from "../../../../assets/icons/aeropress.svg";
+import CoffeeBeansIcon from "../../../../assets/icons/coffee-beans.svg";
+import CoffeeScoopIcon from "../../../../assets/icons/coffee-scoop.svg";
 import { Box } from "../../../ui/atoms/Box";
 import { Text } from "../../../ui/atoms/Text";
 import { useAppTheme } from "../../../ui/theme";
@@ -23,10 +25,43 @@ export const BrewScreen = () => {
 					<Text variant="action">Source: AeroPrecipe.</Text>
 				</Box>
 			</Pressable>
-			<Text variant="body">
-				<AeroPressIcon width={20} height={20} fill={textPrimary} />
-				AeroPress
-			</Text>
+			<Box flexDirection="row" columnGap="s" paddingVertical="s">
+				{/* TODO: Add shadow */}
+				<Box
+					flex={1}
+					alignItems="center"
+					justifyContent="space-between"
+					padding="m"
+					borderRadius={8}
+					borderColor="accentDark"
+					borderWidth={2}
+				>
+					<AeroPressIcon width={48} height={48} fill={textPrimary} />
+					<Text variant="body">AeroPress</Text>
+				</Box>
+				<Box
+					flex={1}
+					alignItems="center"
+					padding="m"
+					borderRadius={8}
+					borderColor="accentDark"
+					borderWidth={2}
+				>
+					<CoffeeBeansIcon width={48} height={48} fill={textPrimary} />
+					<Text variant="body">14g</Text>
+				</Box>
+				<Box
+					flex={1}
+					alignItems="center"
+					padding="m"
+					borderRadius={8}
+					borderColor="accentDark"
+					borderWidth={2}
+				>
+					<CoffeeScoopIcon width={48} height={48} fill={textPrimary} />
+					<Text variant="body">Med-fine</Text>
+				</Box>
+			</Box>
 			<Text variant="body">Filters: two, rinsed</Text>
 			<Text variant="body">Coffee: 14g</Text>
 			<Text variant="body">Grind: Medium-fine (10 clicks on C3)</Text>
