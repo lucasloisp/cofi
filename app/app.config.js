d97596b3e7c5aff4c8e1513f10170f9a513abae4
Lucas Lois <lucasloisp@gmail.com>
2024-04-15 19:40:23 -0300

    chore: removes dev-client

--- app/app.config.js
+++ app/app.config.js
@@ -1,7 +1,5 @@
-const IS_DEV = process.env.APP_VARIANT === "development";
-
 export default {
-	name: IS_DEV ? "Cofi (Dev)" : "Cofi",
+	name: "Cofi",
 	slug: "cofi-companion",
 	version: "1.0.0",
 	orientation: "portrait",
@@ -15,16 +13,14 @@ export default {
 	assetBundlePatterns: ["**/*"],
 	ios: {
 		supportsTablet: true,
-		bundleIdentifier: IS_DEV
-			? "com.lucasloisp.cofi.dev"
-			: "com.lucasloisp.cofi",
+		bundleIdentifier: "com.lucasloisp.cofi",
 	},
 	android: {
 		adaptiveIcon: {
 			foregroundImage: "./assets/adaptive-icon.png",
 			backgroundColor: "#ffffff",
 		},
-		package: IS_DEV ? "com.lucasloisp.cofi.dev" : "com.lucasloisp.cofi",
+		package: "com.lucasloisp.cofi",
 		permissions: ["android.permission.ACCESS_NETWORK_STATE"],
 	},
 	web: {
