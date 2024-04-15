e081bb61b5f7eed13b67abe7121e025d61f3f06a
Lucas Lois <lucasloisp@gmail.com>
2024-04-15 19:35:09 -0300

    Revert "set up react-native-web"
    This reverts commit ba5214cfb3b13efcb4c1b61e997a7dc12e265634.

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,22 +1,4 @@
 module.exports = {
 	root: true,
 	extends: ["universe/native"],
-	overrides: [
-		{
-			files: ["*.js", "*.ts", "*.tsx"],
-			rules: {
-				"no-restricted-imports": [
-					"error",
-					{
-						patterns: [
-							{
-								group: ["@segment/"],
-								message: "Please use the `analytics` service instead.",
-							},
-						],
-					},
-				],
-			},
-		},
-	],
 };
--- app/types.web/react-native-svg.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "react-native-svg" {
-	export type SvgProps = any;
-}
