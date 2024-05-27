4fc4773c9cd864956bdb0e4d34670e5d024b2be8
Lucas Lois <lucasloisp@gmail.com>
2024-05-27 18:08:30 -0300

    removes time on prep tasks

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -20,7 +20,7 @@ const MOCK_RECIPES: Recipe[] = [
 		coffeeGrind: "med_fine",
 		steps: [
 			{ description: "Rinse two filters" },
-			{ time: 0, description: "Bloom 40g of water" },
+			{ description: "Bloom 40g of water" },
 			{ time: 30, description: "Add 180g water" },
 			{ time: 90, description: "Swirl" },
 			{ time: 110, description: "Press for 20 seconds" },
@@ -39,8 +39,8 @@ const MOCK_RECIPES: Recipe[] = [
 		coffeeWeight: 14,
 		coffeeGrind: "fine",
 		steps: [
-			{ time: 0, description: "Add 70g of water." },
-			{ time: 0, description: "Stir for 20 seconds" },
+			{ description: "Add 70g of water." },
+			{ description: "Stir for 20 seconds" },
 			{ time: 20, description: "Press" },
 			{ time: 40, description: "Froth 100ml of milk (60℃)" },
 			{ time: 50, description: "Dilute the coffee" },
@@ -60,7 +60,7 @@ const MOCK_RECIPES: Recipe[] = [
 		coffeeGrind: "med_coarse",
 		steps: [
 			{ description: "Don't be in a Hurry" },
-			{ time: 0, description: "Add 500ml water" },
+			{ description: "Add 500ml water" },
 			{ time: 60 * 4, description: "Stir the crust" },
 			{ time: 60 * 4, description: "Scoop off the foam" },
 			{ time: 60 * 9, description: "Put the plunger in, to the surface" },
