ca903f80bd716fc0af4bcf1eb5c04492fa4ea801
Lucas Lois <lucasloisp@gmail.com>
2024-05-29 09:06:17 -0300

    removes unused fonts object

--- src/ui/fonts.ts
+++ src/ui/fonts.ts
@@ -7,14 +7,6 @@ import {
 	Quicksand_300Light,
 } from "@expo-google-fonts/quicksand";
 
-export const fonts = {
-	Light: "Quicksand_301Light",
-	Regular: "Quicksand_401Regular",
-	Medium: "Quicksand_501Medium",
-	SemiBold: "Quicksand_601SemiBold",
-	Bold: "Quicksand_701Bold",
-};
-
 export const useLoadFonts = () => {
 	return useFonts({
 		QuicksandLight: Quicksand_300Light,
